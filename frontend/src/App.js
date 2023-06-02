61034465cbde7b4838797998563099043692d430
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-06-02 13:38:29 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ethers } from "ethers";
 import abi from "./abi/FundRaiser.json";
 
--- frontend/src/MyComponents/Navbar.js
+++ frontend/src/MyComponents/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./assets/logo.png";
 import metamask from "./assets/MetaMask_Fox.svg.png";
 
