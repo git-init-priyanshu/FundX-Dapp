afaf449ca01f41409bc54630b04c0175ced9cf93
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-06-02 12:27:10 +0530

    Build

--- frontend/src/MyComponents/Navbar.js
+++ frontend/src/MyComponents/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div className="w-full flex justify-between border-b border-neutral-500 items-center sm: mb-4 p-2">
       <div className="flex items-center">
         <img src={logo} alt="" className="w-8 h-8 opacity-80 mr-3" />
-        <p className="logo opacity-80">Fund Raiser</p>
+        <p className="logo opacity-80">FundX</p>
       </div>
       <div>
         <button className="flex bg-blue-900 hover:bg-blue-950 sm: w-28 p-1 rounded-sm items-center">
